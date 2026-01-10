4fa1dcc0541ac066db141c114a6bf7975058f38c
obos-olsen <christian.olsen@obos.no>
2026-01-10 14:16:18 +0100

    Adds links

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Renovering Bad</h1>
+    <main className="min-h-screen flex items-center justify-center bg-black">
+      <div className="flex gap-4">
+        <a
+          href="https://app.sketchup.com/share/tc/europe/dVRTOAqn8ls?source=web&stoken=ioNRCBGs8EmGc96SXaTB4HWuN0ySvKZNUpywfq_B7tfgpVgvtEuu4fMMuT2WmM8w"
+          className="px-8 py-3 bg-white text-black font-semibold rounded hover:bg-gray-200 transition"
+        >
+          Button 1
+        </a>
+        <a
+          href="https://docs.google.com/spreadsheets/d/1BrCCW0B1utHs28P_rEVbSL8-JEt0V-wLZXWx1MIk8-w/edit?usp=sharing"
+          className="px-8 py-3 bg-white text-black font-semibold rounded hover:bg-gray-200 transition"
+        >
+          Button 2
+        </a>
+      </div>
     </main>
   );
 }
