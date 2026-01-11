a0e19222c5b9bc0217980643250a775ad84a1ae2
obos-olsen <christian.olsen@obos.no>
2026-01-11 17:49:17 +0100

    fix meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
+import flisvask from "@/public/flisvask.png";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Renovering Bad",
-  description: "Bathroom renovation project",
+  description: "Renovering Bad",
+  icons: {
+    icon: flisvask.src,
+  },
 };
 
 export default function RootLayout({
